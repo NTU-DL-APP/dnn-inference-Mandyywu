197ecbd42913566251eaadafb243be5be89daa69
您的姓名 <mandyyyyy2222222@gmail.com>
2025-06-10 14:43:10 +0800

    final_answer

--- nn_predict.py
+++ nn_predict.py
@@ -3,12 +3,13 @@ import json
 
 # === Activation functions ===
 def relu(x):
-    # TODO: Implement the Rectified Linear Unit
-    return x
+    # Implement the Rectified Linear Unit
+    return np.maximum(0, x)
 
 def softmax(x):
-    # TODO: Implement the SoftMax function
-    return x
+    # Implement the SoftMax function
+    exp_x = np.exp(x - np.max(x, axis=-1, keepdims=True))  # 防止溢出
+    return exp_x / np.sum(exp_x, axis=-1, keepdims=True)
 
 # === Flatten ===
 def flatten(x):
@@ -41,8 +42,6 @@ def nn_forward_h5(model_arch, weights, data):
 
     return x
 
-
-# You are free to replace nn_forward_h5() with your own implementation 
+# You are free to replace nn_forward_h5() with your own implementation
 def nn_inference(model_arch, weights, data):
     return nn_forward_h5(model_arch, weights, data)
-    
